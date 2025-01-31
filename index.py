2b821c834495b6c896499fd9afe7d32732e12b4e
manan228 <manan228@yahoo.in>
2025-01-31 10:19:19 -0800

    rolling back

--- index.py
+++ index.py
@@ -11,8 +11,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
@@ -24,38 +22,30 @@ message = Mail(
 message.template_id = "d-2891c6a8a72d4a83857fd36807b8841e"
 
 def init_driver():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument("--headless")  # Run in headless mode
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())
-    # service = Service(r"D:\Download\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+    service = Service(r"D:\Download\chromedriver-win64\chromedriver-win64\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=options)
+    
     return driver
 
 def scrape_jobs():
     driver = init_driver()
-
     # URL to scrape
     url = "https://www.orioninc.com/careers/jobs/?keyword=&_job_category=technology-and-engineering&_job_location=north-america"
     driver.get(url)
-
     wait = WebDriverWait(driver, 5)
-
     try:
         decline_button = wait.until(EC.element_to_be_clickable((By.ID, "hs-eu-decline-button")))
         decline_button.click()
-
         print("Cookie popup dismissed.")
-
     except NoSuchElementException:
         print("No cookie popup appeared.")
-
     except TimeoutException:
         print("Timed out waiting for the cookie popup to be clickable.")
-
     # Function to load all jobs by clicking the "Load More" button
     def load_all_jobs():
         while True:
@@ -63,7 +53,6 @@ def scrape_jobs():
                 load_more_button = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.ID, "load-more"))
                 )
-
                 load_more_button.click()
                 print("Load more button clicked")
                 time.sleep(3)
@@ -76,7 +65,7 @@ def scrape_jobs():
 
     # Call the function to load all jobs
     load_all_jobs()
-
+    
     # Extract job details
     jobs_data = []
     jobs = driver.find_elements(By.CLASS_NAME, "teaser-search")
@@ -84,21 +73,18 @@ def scrape_jobs():
         title = job.find_element(By.CLASS_NAME, "article-title").text
         link = job.find_element(By.CLASS_NAME, "article-title").get_attribute("href")
         category = job.find_element(By.CLASS_NAME, "category").text.replace("Category:", "").strip()
-        work_type = job.find_element(By.CLASS_NAME, "work-type").text.replace("Work Type:", "").strip()
-
         jobs_data.append({
                 "title": title,
                 "link": link,
                 "category": category,
-                "work_type": work_type
             })
-
+    
     driver.quit()
-
+    
     message.dynamic_template_data = {
         "jobs": jobs_data
     }
-
+    
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     
